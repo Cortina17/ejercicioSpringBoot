a5dd993e48db1e25d547e977fdbeade46a488924
diego <diego.cortinagarcia@gmail.com>
2022-05-27 12:35:40 +0200

    tables

--- src/main/java/com/capgemini/demo/Equipo.java
+++ src/main/java/com/capgemini/demo/Equipo.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "futbolbd")
+@Table(name = "equipos")
 public class Equipo {
 
 	@Id
--- src/main/java/com/capgemini/demo/Jugador.java
+++ src/main/java/com/capgemini/demo/Jugador.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "/")
+@Table(name = "/jugadores")
 public class Jugador {
 
 	@Id
