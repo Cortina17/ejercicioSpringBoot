999d42b33e1ae5a4c5675b2a7b6fb832b3c99295
diego <diego.cortinagarcia@gmail.com>
2022-05-27 12:22:57 +0200

    equipo class

---
+++ src/main/java/com/capgemini/demo/Equipo.java
@@ -0,0 +1,36 @@
+package com.capgemini.demo;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Equipo {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private String nombre;
+
+	public void mostrar() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
