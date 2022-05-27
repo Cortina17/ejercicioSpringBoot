58ecc388df6c66088f3dbdded7a1e3b6f55e3e3f
diego <diego.cortinagarcia@gmail.com>
2022-05-27 12:32:22 +0200

    classes

---
+++ src/main/java/com/capgemini/demo/Camiseta.java
@@ -0,0 +1,49 @@
+package com.capgemini.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "/")
+public class Camiseta {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private int numero;
+
+	@Column
+	private Marca marca;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+}
---
+++ src/main/java/com/capgemini/demo/Jugador.java
@@ -0,0 +1,62 @@
+package com.capgemini.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "/")
+public class Jugador {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private String nombre;
+
+	@Column
+	private Equipo equipo;
+	
+	@Column
+	private Camiseta camiseta;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+
+	public Camiseta getCamiseta() {
+		return camiseta;
+	}
+
+	public void setCamiseta(Camiseta camiseta) {
+		this.camiseta = camiseta;
+	}
+
+
+
+}
